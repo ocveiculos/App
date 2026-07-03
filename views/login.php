cafc7a5a0865e34aa9dd14d7b89b427ec6478252
ocveiculos <ocveiculos@hotmail.com>
2026-07-02 22:32:27 -0300

    Primeiro Commit

---
+++ views/login.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Olívia Carneiro Veículos</title>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+    />
+    <link
+      rel="shortcut icon"
+      href="/assets/logo_oc.jpeg"
+      type="/assets/logo_oc.jpeg"
+    />
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+  <body>
+    <main class="container">
+      <form>
+        <h1>Entre na Plataforma</h1>
+        <div class="input-box">
+          <input placeholder="Usuário" type="email" />
+          <i class="bx bxs-user"></i>
+        </div>
+        <div class="input-box">
+          <input placeholder="Senha" type="password" />
+          <i class="bx bxs-lock-alt"></i>
+        </div>
+        <div class="remember-forgot">
+          <label for="">
+            <input type="checkbox" />
+            Lembrar minha Senha
+          </label>
+        </div>
+        <button type="submit" class="login">Login</button>
+      </form>
+    </main>
+  </body>
+</html>
